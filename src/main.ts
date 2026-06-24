b051e7a2669456272a7ffcf4e58827ba9a22aa79
Alaa-fawaz-Manea-AF <alaafawaz010011@gmail.com>
2026-06-24 17:33:54 +0300

    update dark mode lang

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(morgan('dev'));
   app.use(json());
-  // const allowedOrigins = [process.env.CLIENT_URL];
-  const allowedOrigins = ['http://localhost:3000'];
+  const allowedOrigins = [process.env.CLIENT_URL];
   app.use(json());
   app.enableCors({
     origin: (origin, callback) => {
